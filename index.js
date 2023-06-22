2d128b0f2164f79375a03816b18bdc6dcaf6cd65
Victor <victornjoroge2020@gmail.com>
2023-06-22 11:27:29 +0300

    done

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
+ return (fetch("https://anapioficeandfire.com/api/books"))
+  .then((res) =>{
+    res.json()
+
+    .then((json) =>{
+      renderBooks(json)
+
+      console.log(json)
+    })
+  })
   
 }
 
